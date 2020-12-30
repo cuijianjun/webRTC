61b2698dd11a87c8b11c2c2c2c4150ad860dd60b
Frank <376247934@qq.com>
2020-12-30 18:50:50 +0800

    'start chatroom'

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git add .
 git commit -m  "$1"
 
 
-# git push origin master
+git push origin master
 
-# ssh -p 22 root@124.70.208.100 "cd ~/webRTC/; git pull origin master; npm i ;pwd;cd webserver/;pwd; forever stop server.js;forever start server.js"
+ssh -p 22 root@124.70.208.100 "cd ~/webRTC/; git pull origin master; npm i ;pwd;cd webserver/;pwd; forever stop server.js;forever start server.js"
 
