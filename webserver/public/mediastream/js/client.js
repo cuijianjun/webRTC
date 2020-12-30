be1f926b4f6815a4422c682721737d47219d3120
Frank <376247934@qq.com>
2020-12-30 15:33:49 +0800

    只采集音频

--- webserver/public/mediastream/js/client.js
+++ webserver/public/mediastream/js/client.js
@@ -3,11 +3,14 @@
 var audioSource = document.querySelector("select#audioSource");
 var audioOutput = document.querySelector("select#audioOutput");
 var videoSource = document.querySelector("select#videoSource");
-var videoplay = document.querySelector("video#player");
-var filtersSelect = document.querySelector('select#filter')
+
+// var videoplay = document.querySelector("video#player");
+var audioplay = document.querySelector("audio#audioplayer");
+
+var filtersSelect = document.querySelector("select#filter");
 // picture
-var snapshot = document.querySelector('button#snapshot');
-var picture = document.querySelector('canvas#picture');
+var snapshot = document.querySelector("button#snapshot");
+var picture = document.querySelector("canvas#picture");
 picture.width = 320;
 picture.height = 240;
 function start() {
@@ -15,28 +18,26 @@ function start() {
     console.log("getUserMedia is not supported");
     return;
   } else {
-    var deviceId = videoSource.value;  
+    var deviceId = videoSource.value;
     var constraints = {
-      video: {
-        deviceId: deviceId? deviceId : undefined,
-        width: {
-          min: 300,
-          max: 640,
-        },
-        height: {
-          min: 300,
-          max: 480,
-        },
-        frameRate: {
-          min: 15,
-          max: 30,
-        },
-        facingMode: "enviroment",
-      },
-      audio: {
-        noiseSuppression: true, // 降噪
-        echoCancellation: true, // 回音消除
-      },
+      //   video: {
+      //     deviceId: deviceId? deviceId : undefined,
+      //     width: {
+      //       min: 300,
+      //       max: 640,
+      //     },
+      //     height: {
+      //       min: 300,
+      //       max: 480,
+      //     },
+      //     frameRate: {
+      //       min: 15,
+      //       max: 30,
+      //     },
+      //     facingMode: "enviroment",
+      //   },
+      video: false,
+      audio: true,
     };
     navigator.mediaDevices
       .getUserMedia(constraints)
@@ -45,20 +46,23 @@ function start() {
       .catch(handleError);
   }
 }
-start()
+start();
 
-videoSource.onchange = start
-filtersSelect.onchange = function() {
-    videoplay.className = filtersSelect.value
-}
+videoSource.onchange = start;
+filtersSelect.onchange = function () {
+  videoplay.className = filtersSelect.value;
+};
 snapshot.onclick = function () {
-    picture.className = filtersSelect.value
-    picture.getContext('2d').drawImage(videoplay,0,0,picture.width,picture.height)
-}
+  picture.className = filtersSelect.value;
+  picture
+    .getContext("2d")
+    .drawImage(videoplay, 0, 0, picture.width, picture.height);
+};
 
 function gotMediaStream(stream) {
   console.log("stream", stream);
-  videoplay.srcObject = stream;
+  //   videoplay.srcObject = stream;
+  audioplay.srcObject = stream;
   return navigator.mediaDevices.enumerateDevices();
 }
 
