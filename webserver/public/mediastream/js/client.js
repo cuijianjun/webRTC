14ab6b006694e67a992f2eef20c7e8bcb9b5f7af
Frank <376247934@qq.com>
2020-12-30 15:25:40 +0800

    从视频中获取图片

--- webserver/public/mediastream/js/client.js
+++ webserver/public/mediastream/js/client.js
@@ -5,6 +5,11 @@ var audioOutput = document.querySelector("select#audioOutput");
 var videoSource = document.querySelector("select#videoSource");
 var videoplay = document.querySelector("video#player");
 var filtersSelect = document.querySelector('select#filter')
+// picture
+var snapshot = document.querySelector('button#snapshot');
+var picture = document.querySelector('canvas#picture');
+picture.width = 320;
+picture.height = 240;
 function start() {
   if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
     console.log("getUserMedia is not supported");
@@ -45,6 +50,10 @@ start()
 videoSource.onchange = start
 filtersSelect.onchange = function() {
     videoplay.className = filtersSelect.value
+}
+snapshot.onclick = function () {
+    picture.className = filtersSelect.value
+    picture.getContext('2d').drawImage(videoplay,0,0,picture.width,picture.height)
 }
 
 function gotMediaStream(stream) {
