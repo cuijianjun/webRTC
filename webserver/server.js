fb81bc56e0a4d7f588fc294de1be6fc138206caf
Frank <376247934@qq.com>
2020-12-31 00:32:37 +0800

    'finish chatroom'

--- webserver/server.js
+++ webserver/server.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var express = require('express');
 var serveIndex = require('serve-index');
 
-var socketIo = require('socket.io')
+var socketIo = require('socket.io');
 var log4js = require('log4js');
 log4js.configure({
 	appenders: {
@@ -41,27 +41,33 @@ var options = {
 }
 // https server 
 var https_server = https.createServer(options, app);
-var io = socketIo.listen(https_server)
+var io = socketIo(https_server)
 io.sockets.on('connection', (socket) => {
 	socket.on('join', (room) => {
 		socket.join(room)
-		var myRoom = io.sockets.adapter.rooms[room];
-		var users = Object.keys(myRoom.sockets).length;
-		logger.log('the number of user in room is: ' + users);
-		// socket.emit('joined', room, socket.id) 单人回复
+		var myRoom = io.sockets.adapter.rooms.get(room);
+		var users = myRoom.size;
+		console.log("users", users);
+		// logger.log('the number of user in room is: ' + users);
+		// socket.emit('joined', room, socket.id) // 单人回复
 		// socket.to(room).emit('joined', room, socket.id) // 除自己外的
 		// io.in(room).emit('joined', room, socket.id) // 房间内所有人
 		socket.broadcast.emit('joined', room, socket.id) // 除自己，全部站点
 	}) 
-	socket.on('join', (room) => {
-		var myRoom = io.sockets.adapter.rooms[room];
-		var users = Object.keys(myRoom.sockets).length;
-		logger.log('the number of user in room is: ' + (users - 1));
+	socket.on('leave', (room) => {
+		var myRoom = io.sockets.adapter.rooms.get(room);
+		var users = myRoom.size;
+		logger.log('the number of user in room is: ' + users);
 		socket.leave(room)
 		// socket.emit('joined', room, socket.id) 单人回复
 		// socket.to(room).emit('joined', room, socket.id) // 除自己外的
 		// io.in(room).emit('joined', room, socket.id) // 房间内所有人
 		socket.broadcast.emit('joined', room, socket.id) // 除自己，全部站点
 	})
+
+	socket.on('message',(room, data)=>{
+		console.log("broadcast", room, data);
+        socket.broadcast.emit('message', room, data)
+    })
 })
 https_server.listen(444, '0.0.0.0')
