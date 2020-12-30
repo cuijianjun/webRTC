b94e0303bbf5d917abb635a11f16cb70ad734578
Frank <376247934@qq.com>
2020-12-30 15:12:58 +0800

    视频渲染效果

--- webserver/public/mediastream/js/client.js
+++ webserver/public/mediastream/js/client.js
@@ -1,20 +1,73 @@
-'use strict'
-var videoplay = document.querySelector('video#player');
-if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+"use strict";
+
+var audioSource = document.querySelector("select#audioSource");
+var audioOutput = document.querySelector("select#audioOutput");
+var videoSource = document.querySelector("select#videoSource");
+var videoplay = document.querySelector("video#player");
+var filtersSelect = document.querySelector('select#filter')
+function start() {
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
     console.log("getUserMedia is not supported");
-} else {
+    return;
+  } else {
+    var deviceId = videoSource.value;  
     var constraints = {
-        video: true,
-        audio: true
-    }
-    navigator.mediaDevices.getUserMedia(constraints).then(gotMediaStream).catch(handleError)
+      video: {
+        deviceId: deviceId? deviceId : undefined,
+        width: {
+          min: 300,
+          max: 640,
+        },
+        height: {
+          min: 300,
+          max: 480,
+        },
+        frameRate: {
+          min: 15,
+          max: 30,
+        },
+        facingMode: "enviroment",
+      },
+      audio: {
+        noiseSuppression: true, // 降噪
+        echoCancellation: true, // 回音消除
+      },
+    };
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(gotMediaStream)
+      .then(gotDevices)
+      .catch(handleError);
+  }
+}
+start()
+
+videoSource.onchange = start
+filtersSelect.onchange = function() {
+    videoplay.className = filtersSelect.value
 }
 
 function gotMediaStream(stream) {
-    console.log("stream", stream);
-    videoplay.srcObject = stream
-}
-function handleError (err) {
-    console.log("getUserMedia error:", err);
+  console.log("stream", stream);
+  videoplay.srcObject = stream;
+  return navigator.mediaDevices.enumerateDevices();
 }
 
+function gotDevices(deviceInfos) {
+  console.log("deviceInfos", deviceInfos);
+  deviceInfos.forEach((deviceInfo) => {
+    var option = document.createElement("option");
+    option.text = deviceInfo.label;
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === "audioinput") {
+      audioSource.appendChild(option);
+    } else if (deviceInfo.kind === "audiooutput") {
+      audioOutput.appendChild(option);
+    } else if (deviceInfo.kind === "videoinput ") {
+      videoSource.appendChild(option);
+    }
+  });
+}
+function handleError(err) {
+  console.log("getUserMedia error:", err);
+}
